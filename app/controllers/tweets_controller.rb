aab14b3d586787b8ad73dd3c16c792e3c7b8657d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-25 23:55:10 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -49,6 +49,27 @@ class TweetsController < ApplicationController
   end
 
   patch '/tweets/:id' do
+  if logged_in?
+    if params[:content] == ""
+      redirect "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      if session[:user_id] = @tweet.user_id
+        if @tweet.update(content: params[:content])
+          redirect  "/tweets/#{@tweet.id}"
+        else
+          redirect "/tweets/#{@tweet.id}/edit"
+        end
+      else
+        redirect '/tweets'
+      end
+    end
+  else
+    redirect '/login'
+  end
+end
+
+  patch '/tweets/:id' do
     if logged_in?
       if params[:content].empty?
         redirect "/tweets/#{params[:id]}/edit"
