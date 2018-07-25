810dddc5090e0b90c79c2c623ef69b1edda22e6f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-25 23:21:05 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,7 +38,7 @@ class TweetsController < ApplicationController
 
   get '/tweets/:id/edit' do
     if logged_in?
-      @tweet = Tweet.find(params[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       if !session[:user_id] == @tweet.user_id
         redirect '/tweets'
       else
